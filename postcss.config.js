a8639204570287d5f546e4f56ab7aeb1a79ddb03
mayankrsagar <mayankrsagar@gmail.com>
2025-07-14 15:10:55 +0530

    fixing

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),  // the Tailwind PostCSS integration
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
